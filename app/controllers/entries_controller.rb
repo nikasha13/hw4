09bbc0e896fb06380db0bd499b75e5908fcb7ca2
Nikasha Patel <nikasha.patel@kellogg.northwestern.edu>
2025-05-29 20:21:11 +0000

    completed week9 tasks

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,13 +4,19 @@ class EntriesController < ApplicationController
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    @user = User.find_by({"id" => session["user_id"]})
+    if @user != nil
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry["user_id"] = @user["id"] 
+      @entry.save
+      redirect_to "/places/#{@entry["place_id"]}"
+    else
+      redirect_to "/login"
+    end
   end
 
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,18 +5,24 @@ class PlacesController < ApplicationController
   end
 
   def show
-    @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"] })
+    @place = Place.find_by({ "id" => params["id"]})
+    @entries = Entry.where({ "place_id" => @place["id"], "user_id" => session["user_id"] })
   end
 
   def new
+    @place = Place.find_by({ "id" => params["place_id"] })
   end
 
   def create
+    @user = User.find_by({"id" => session["user_id"]})
+    if @user != nil
     @place = Place.new
-    @place["name"] = params["name"]
+    @place["name"] = params["name"] 
     @place.save
     redirect_to "/places"
+    else
+      redirect_to "/login"
+    end
   end
 
 end
